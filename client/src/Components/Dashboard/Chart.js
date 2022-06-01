592b5c20b9799f77b8b6660c0275d14a64766311
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-06-01 11:01:13 -0700

    chart displays with tooltip to see exact amount

--- client/src/Components/Dashboard/Chart.js
+++ client/src/Components/Dashboard/Chart.js
@@ -6,11 +6,29 @@ import {
   ValueAxis,
   Chart,
   LineSeries,
+  BarSeries,
+  Tooltip,
 } from '@devexpress/dx-react-chart-material-ui';
+import { EventTracker } from '@devexpress/dx-react-chart';
 
 function createData(month, amount) {
   return { x: month, y: amount };
 }
+
+// export default function FutureProjections({ data }) {
+//   return (
+//     <Paper>
+//       <Chart data={data}>
+//         <ArgumentAxis />
+//         <ValueAxis />
+
+//         <BarSeries valueField="balance" argumentField="month" />
+//         <EventTracker />
+//         <Tooltip />
+//       </Chart>
+//     </Paper>
+//   );
+// }
 
 export default function FutureProjections({ data }) {
   return (
@@ -21,6 +39,8 @@ export default function FutureProjections({ data }) {
         <ValueAxis />
 
         <LineSeries valueField="y" argumentField="x" />
+        <EventTracker />
+        <Tooltip />
       </Chart>
     </Paper>
   );
