b742b4b24d20fbc10c0b804a4ba7752d42fb9f3f
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-07-07 20:45:41 -0700

    scenarios modal shows all scenarios and info about them

--- client/src/Components/Scenarios/Scenarios.js
+++ client/src/Components/Scenarios/Scenarios.js
@@ -5,6 +5,12 @@ import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { setScenarios } from '../../Store/Scenarios';
+import { Context } from '../ContextProvider';
+import Paper from '@mui/material/Paper';
+import Title from '../Dashboard/Title';
+import { Grid, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const style = {
   position: 'absolute',
@@ -12,21 +18,23 @@ const style = {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: '65vw',
-  height: '65vh',
+  height: '75vh',
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 2,
   p: 4,
   display: 'flex',
   flexDirection: 'column',
-  justifyContent: 'center',
   alignItems: 'center',
+  overflow: 'scroll',
 };
 
 export default function Scenarios() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const { state, stateScenarios } = React.useContext(Context);
+  const [exitColor, setExitColor] = React.useState('white');
 
   return (
     <div>
@@ -45,13 +53,77 @@ export default function Scenarios() {
         }}
       >
         <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
+          <Box id="scenarios" sx={style}>
+            <Typography
+              sx={{ fontSize: '2rem', color: '#90caf9', marginBottom: '1rem' }}
+            >
+              Scenarios
             </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
+            <div className="display-column">
+              {stateScenarios?.map((scenario) => {
+                let specialDateO = scenario.specialDate
+                  ? new Date(scenario.specialDate)
+                  : null;
+                let changeDateO = scenario.changeDate
+                  ? new Date(scenario.changeDate)
+                  : null;
+
+                return (
+                  <Paper
+                    sx={{
+                      p: 2,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      height: 'fit-content',
+                      width: '75vh',
+                      marginBottom: '1rem',
+                    }}
+                  >
+                    <Title>{scenario?.name}</Title>
+
+                    {!scenario?.specialAmount ? null : (
+                      <div
+                        className="display-column"
+                        style={{
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}
+                      >
+                        <Title>Special Assessment</Title>
+                        <Typography style={{ fontSize: '1.3rem' }}>
+                          {`$${scenario?.specialAmount} on ${
+                            specialDateO.getMonth() + 1
+                          }/${
+                            specialDateO.getDate() + 1
+                          }/${specialDateO.getFullYear()}`}
+                        </Typography>
+                      </div>
+                    )}
+
+                    {!scenario.changeAmount ? null : (
+                      <div
+                        className="display-column"
+                        style={{
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}
+                      >
+                        <Title>Assessments Change</Title>
+                        <Typography style={{ fontSize: '1.3rem' }}>
+                          {`$${scenario?.changeAmount} beginning ${
+                            changeDateO?.getMonth() + 1
+                          }/${
+                            changeDateO.getDate() + 1
+                          }/${changeDateO.getFullYear()}`}
+                        </Typography>
+                      </div>
+                    )}
+                  </Paper>
+                );
+              })}
+            </div>
           </Box>
         </Fade>
       </Modal>
