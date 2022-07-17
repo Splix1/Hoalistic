4a19c52b52e6f3f922196676cf7badd4f527824f
Splix1 <86242483+Splix1@users.noreply.github.com>
2022-07-17 13:40:15 -0700

    editing units uses modal

---
+++ client/src/Components/Units/EditUnit.js
@@ -0,0 +1,217 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Title from '../Dashboard/Title';
+import TextField from '@mui/material/TextField';
+import { Context } from '../ContextProvider';
+import CurrencyInput from 'react-currency-input-field';
+import supabase from '../../client';
+import { setUnits } from '../../Store/Units';
+import { IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '65vw',
+  height: 'fit-content',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 2,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+export default function EditUnit({ unit }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const { state, stateUnits, dispatchUnits } = React.useContext(Context);
+  const [tenant_name, setTenantName] = React.useState(unit?.tenant_name);
+  const [monthly_assessment, setMonthlyAssessment] = React.useState(
+    unit?.monthly_assessment
+  );
+  const [unitID, setUnitID] = React.useState(unit?.unitID);
+  const [dateMovedIn, setDateMovedIn] = React.useState(unit?.dateMovedIn);
+  const [exitColor, setExitColor] = React.useState('white');
+
+  async function updateUnit() {
+    if (
+      !unitID ||
+      !monthly_assessment ||
+      !monthly_assessment ||
+      !monthly_assessment
+    ) {
+      alert('All fields must be fulfilled.');
+      return;
+    }
+    let { data: updatedUnit, error } = await supabase
+      .from('Units')
+      .update({
+        unitID,
+        monthly_assessment,
+        tenant_name,
+        dateMovedIn,
+      })
+      .eq('id', unit?.id);
+    if (error) {
+      alert('There was a problem updating this unit');
+      return;
+    }
+
+    dispatchUnits(
+      setUnits(
+        stateUnits?.map((currentUnit) => {
+          if (currentUnit.id === updatedUnit[0].id) return updatedUnit[0];
+          return currentUnit;
+        })
+      )
+    );
+    setOpen(false);
+  }
+
+  return (
+    <div>
+      <Button
+        onClick={handleOpen}
+        fullWidth
+        variant="contained"
+        style={{
+          width: 'fit-content',
+          marginTop: '1rem',
+          marginRight: '0.5rem',
+        }}
+      >
+        edit
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <IconButton
+              onClick={() => setOpen(false)}
+              id="x"
+              onMouseEnter={() => setExitColor('red')}
+              onMouseLeave={() => setExitColor('white')}
+              style={{
+                color: exitColor,
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+            <Title>Unit ID</Title>
+            <TextField
+              required
+              id="scenarioName"
+              label="Unit #"
+              name="scenarioName"
+              defaultValue={unit?.unitID}
+              style={{ marginBottom: '1rem', marginTop: '0.5rem' }}
+              onChange={(evt) => setUnitID(evt.target.value)}
+            />
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                marginTop: '0.5rem',
+                marginBottom: '1rem',
+              }}
+            >
+              <div className="display-column" style={{ alignItems: 'center' }}>
+                <Title>Monthly Assessment</Title>
+                <CurrencyInput
+                  prefix="$"
+                  placeholder="Assessment Amount"
+                  decimalsLimit={2}
+                  defaultValue={unit?.monthly_assessment}
+                  style={{
+                    height: '3.5rem',
+                    fontSize: '1rem',
+                    color: state?.theme === 'light' ? '#121212' : 'white',
+                    marginRight: '1rem',
+                    backgroundColor:
+                      state?.theme === 'light' ? 'white' : '#121212',
+                  }}
+                  onValueChange={(value) => setMonthlyAssessment(value)}
+                />
+              </div>
+            </div>
+
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                marginTop: '0.5rem',
+              }}
+            >
+              <div className="display-column" style={{ alignItems: 'center' }}>
+                <Title>Tenant Name</Title>
+                <TextField
+                  required
+                  id="scenarioName"
+                  label="Tenant Name"
+                  name="scenarioName"
+                  defaultValue={unit?.tenant_name}
+                  style={{ marginRight: '1rem' }}
+                  onChange={(evt) => setTenantName(evt.target.value)}
+                />
+              </div>
+              <div className="disply-column" style={{ alignItems: 'center' }}>
+                <Title>Date Moved In</Title>
+                <TextField
+                  type={'date'}
+                  required
+                  fullWidth
+                  id="beginDate"
+                  name="beginDate"
+                  autoComplete="Jimmy"
+                  defaultValue={unit?.dateMovedIn}
+                  onChange={(evt) => setDateMovedIn(evt.target.value)}
+                />
+              </div>
+            </div>
+            <div id="form-input" style={{ marginTop: '1rem' }}>
+              <Button
+                variant="contained"
+                style={{ marginRight: '1rem', marginTop: '1rem' }}
+                onClick={updateUnit}
+              >
+                Finish
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => setOpen(false)}
+                style={{ marginRight: '1rem', marginTop: '1rem' }}
+              >
+                Cancel
+              </Button>
+            </div>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
--- client/src/Components/Units/SingleUnit.js
+++ client/src/Components/Units/SingleUnit.js
@@ -8,44 +8,16 @@ import supabase from '../../client';
 import DeletingUnit from './DeletingUnit';
 import { Context } from '../ContextProvider';
 import { setUnits } from '../../Store/Units';
+import EditUnit from './EditUnit';
 const dayjs = require('dayjs');
 
 const mdTheme = createTheme();
 
 function SingleUnit({ creatingUnit, theUnit }) {
-  let { dateMovedIn, monthly_assessment, tenant_name, unitID } = theUnit;
-  let [newUnitID, setNewUnitID] = useState(unitID);
-  let [tenantName, setTenantName] = useState(tenant_name);
-  let [monthlyAssessment, setMonthlyAssessment] = useState(monthly_assessment);
-  let [movedIn, setMovedIn] = useState(dateMovedIn);
-  let [editingUnit, setEditingUnit] = useState(false);
   let [unit, setUnit] = useState(theUnit);
   let [deletingUnit, setDeletingUnit] = useState(false);
   let { state, stateUnits, dispatchUnits } = useContext(Context);
   let [unitDate, setUnitDate] = useState(dayjs(theUnit?.dateMovedIn));
-
-  async function updateUnit() {
-    if (!newUnitID || !monthlyAssessment || !tenantName || !movedIn) {
-      alert('All fields must be fulfilled.');
-      return;
-    }
-    let { data: updatedUnit, error } = await supabase
-      .from('Units')
-      .update({
-        unitID: newUnitID,
-        monthly_assessment: monthlyAssessment,
-        tenant_name: tenantName,
-        dateMovedIn: movedIn,
-      })
-      .eq('id', theUnit?.id);
-    if (error) {
-      alert('There was a problem updating this unit');
-      return;
-    }
-
-    setUnit(updatedUnit[0]);
-    setEditingUnit(false);
-  }
 
   async function deleteUnit() {
     let { data } = await supabase.from('Units').delete().eq('id', unit?.id);
@@ -71,111 +43,32 @@ function SingleUnit({ creatingUnit, theUnit }) {
           justifyContent: 'flex-start',
         }}
       >
-        {!editingUnit ? (
-          <div className="single-unit">
-            <Typography sx={{ fontSize: '1.5rem' }}>
-              Unit: {unit?.unitID}
-            </Typography>
-            <Typography sx={{ fontSize: '1.5rem' }}>
-              Tenant Name: {unit?.tenant_name}
-            </Typography>
-            <Typography sx={{ fontSize: '1.5rem' }}>
-              Monthly Assessment: ${numberWithCommas(unit?.monthly_assessment)}
-            </Typography>
-            <Typography sx={{ fontSize: '1.5rem' }}>
-              Date moved in:{' '}
-              {`${unitDate?.$M + 1}/${unitDate?.$D}/${unitDate?.$y}`}
-            </Typography>
-            <div className="display-row">
-              <Button
-                variant="contained"
-                onClick={() => setEditingUnit(true)}
-                style={{ marginRight: '1rem', marginTop: '1rem' }}
-              >
-                edit
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => setDeletingUnit(true)}
-                style={{ marginRight: '1rem', marginTop: '1rem' }}
-              >
-                delete
-              </Button>
-            </div>
+        <div className="single-unit">
+          <Typography sx={{ fontSize: '1.5rem' }}>
+            Unit: {theUnit?.unitID}
+          </Typography>
+          <Typography sx={{ fontSize: '1.5rem' }}>
+            Tenant Name: {theUnit?.tenant_name}
+          </Typography>
+          <Typography sx={{ fontSize: '1.5rem' }}>
+            Monthly Assessment: ${numberWithCommas(theUnit?.monthly_assessment)}
+          </Typography>
+          <Typography sx={{ fontSize: '1.5rem' }}>
+            Date moved in:{' '}
+            {`${unitDate?.$M + 1}/${unitDate?.$D}/${unitDate?.$y}`}
+          </Typography>
+          <div className="display-row">
+            <EditUnit unit={theUnit} />
+            <Button
+              variant="contained"
+              onClick={() => setDeletingUnit(true)}
+              style={{ marginRight: '1rem', marginTop: '1rem' }}
+            >
+              delete
+            </Button>
           </div>
-        ) : (
-          <div className="single-unit">
-            <TextField
-              required
-              fullWidth
-              id="unitNumber"
-              label="Unit #"
-              name="unitNumber"
-              defaultValue={unit?.unitID}
-              autoComplete="1A"
-              className="editing-unit"
-              onChange={(evt) => setNewUnitID(evt.target.value)}
-              style={{ marginBottom: '0.5rem' }}
-            />
-            <TextField
-              required
-              fullWidth
-              id="name"
-              label="Tenant Name"
-              name="name"
-              autoComplete="Jimmy"
-              defaultValue={unit?.tenant_name}
-              className="editing-unit"
-              onChange={(evt) => setTenantName(evt.target.value)}
-              style={{ marginBottom: '0.5rem' }}
-            />
-            <CurrencyInput
-              id="monthlyAssessment"
-              name="monthlyAssessment"
-              prefix="$"
-              placeholder="Monthly Assessment"
-              defaultValue={unit?.monthly_assessment}
-              decimalsLimit={2}
-              style={{
-                height: '3rem',
-                fontSize: '1rem',
-                backgroundColor: '#121212',
-                color: 'white',
-                marginBottom: '0.5rem',
-              }}
-              onValueChange={(value) => setMonthlyAssessment(value)}
-              className="editing-unit"
-            />
-            <TextField
-              type={'date'}
-              required
-              fullWidth
-              id="dateMovedIn"
-              name="dateMovedIn"
-              autoComplete="Jimmy"
-              defaultValue={unit?.dateMovedIn}
-              className="editing-unit"
-              onChange={(evt) => setMovedIn(evt.target.value)}
-              style={{ marginBottom: '0.5rem' }}
-            />
-            <div className="display-row">
-              <Button
-                variant="contained"
-                style={{ marginRight: '1rem', marginTop: '1rem' }}
-                onClick={updateUnit}
-              >
-                Save
-              </Button>
-              <Button
-                style={{ marginRight: '1rem', marginTop: '1rem' }}
-                variant="contained"
-                onClick={() => setEditingUnit(false)}
-              >
-                Cancel
-              </Button>
-            </div>
-          </div>
-        )}
+        </div>
+
         {deletingUnit ? (
           <DeletingUnit
             setDeletingUnit={setDeletingUnit}
